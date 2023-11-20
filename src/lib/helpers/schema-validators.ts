285b7838b102228a86fc656157563d632b7a1404
Adrian <alextravc.342@gmail.com>
2023-11-20 12:29:34 -0500

    [ADD]: SignupSchema

--- src/lib/helpers/schema-validators.ts
+++ src/lib/helpers/schema-validators.ts
@@ -1,9 +1,24 @@
 import { z } from 'zod';
 
-const getFormSchema = () =>
-  z.object({
-    email: z.string().describe('Email').email({ message: 'Invalid Email' }),
-    password: z.string().describe('Password').min(1, 'Password is required'),
-  });
+export const LoginSchema = z.object({
+  email: z.string().describe('Email').email({ message: 'Invalid Email' }),
+  password: z.string().describe('Password').min(1, 'Password is required'),
+});
 
-export const LoginSchema = getFormSchema();
+export const SignupSchema = z
+  .object({
+    email: z.string().describe('Email').email({ message: 'Invalid Email' }),
+    password: z
+      .string()
+      .describe('Password')
+      .min(6, 'Password must be minimum 6 characters'),
+    confirmPassword: z
+      .string()
+      .describe('Confirm Password')
+      .min(6, 'Password must be minimum 6 characters'),
+  })
+  .refine(data => data.password === data.confirmPassword, {
+    // if not
+    message: "Passwords don't match.",
+    path: ['confirmPassword'], // set error to confirmPassword
+  });
