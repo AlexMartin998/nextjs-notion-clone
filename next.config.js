c04e05bfce9831a8edc240c23962a88416946e82
Adrian <alextravc.342@gmail.com>
2023-12-01 13:01:07 -0500

    [ADD]: allow external source images

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // allow external source images
+  images: {
+    domains: ['jkvwuvvfqyqrcyxfgfef.supabase.co'], // env - NEXT_PUBLIC_SUPABASE_URL
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
