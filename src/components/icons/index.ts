ace5d3f0b817e3b2818d9a9ecf874a7f6ca80ca8
Adrian <alextravc.342@gmail.com>
2023-12-10 15:48:58 -0500

    [ADD]: CypressTrashIcon

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,2 +1,4 @@
 export { default as CypressDiamondIcon } from './CypressDiamondIcon';
 export { default as CypressHomeIcon } from './CypressHomeIcon';
+export { default as CypressSettingsIcon } from './CypressSettingsIcon';
+export { default as CypressTrashIcon } from './CypressTrashIcon';
