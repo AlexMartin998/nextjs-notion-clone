1730aeea700ff7c7a9ce02b2b448d6029441010f
Adrian <alextravc.342@gmail.com>
2023-12-12 09:54:22 -0500

    [ADD]: FilePage includes QuillEditor

--- src/app/(main)/dashboard/[workspaceId]/[folderId]/[fileId]/page.tsx
+++ src/app/(main)/dashboard/[workspaceId]/[folderId]/[fileId]/page.tsx
@@ -1,9 +1,27 @@
-export type FilePageProps = {};
+export const dynamic = 'force-dynamic'; // works with quill
 
-const FilePage: React.FC<FilePageProps> = () => {
+import { redirect } from 'next/navigation';
 
-  return <>FilePage</>;
+import QuillEditor from '@/components/quill-editor/QuillEditor';
+import { getFolderDetails } from '@/lib/supabase/queries';
 
+export type FilePageProps = {
+  params: { fileId: string };
+};
+
+const FilePage: React.FC<FilePageProps> = async ({ params }) => {
+  const { data, error } = await getFolderDetails(params.fileId);
+  if (error || !data.length) redirect('/dashboard');
+
+  return (
+    <div className="relative ">
+      <QuillEditor
+        dirType="file"
+        fileId={params.fileId}
+        dirDetails={data[0] || {}}
+      />
+    </div>
+  );
 };
 
 export default FilePage;
