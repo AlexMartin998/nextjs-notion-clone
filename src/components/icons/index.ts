ceb80098987c7379dd247815751aa15a4c861f19
Adrian <alextravc.342@gmail.com>
2023-12-12 23:23:20 -0500

    [ADD]: CypressProfileIcon

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,4 +1,5 @@
 export { default as CypressDiamondIcon } from './CypressDiamondIcon';
 export { default as CypressHomeIcon } from './CypressHomeIcon';
+export { default as CypressProfileIcon } from './CypressProfileIcon';
 export { default as CypressSettingsIcon } from './CypressSettingsIcon';
 export { default as CypressTrashIcon } from './CypressTrashIcon';
