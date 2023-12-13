f2e61eb544e41326dfbdf2b7ed01d04088297384
Adrian <alextravc.342@gmail.com>
2023-12-13 10:02:57 -0500

    [ADD]: SubscriptionModalProvider

--- src/app/(main)/dashboard/layout.tsx
+++ src/app/(main)/dashboard/layout.tsx
@@ -1,10 +1,22 @@
+import { SubscriptionModalProvider } from '@/lib/context/ui/subscription-modal-provider';
+import { getActiveProductsWithPrice } from '@/lib/supabase/queries';
+
 export type MainLayoutProps = {
   children: React.ReactNode;
   params: any;
 };
 
-const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
-  return <main className="flex overflow-hidden h-screen">{children}</main>;
+const MainLayout: React.FC<MainLayoutProps> = async ({ children }) => {
+  const { data: products, error } = await getActiveProductsWithPrice();
+  if (error) throw new Error();
+
+  return (
+    <main className="flex over-hidden h-screen">
+      <SubscriptionModalProvider products={products}>
+        {children}
+      </SubscriptionModalProvider>
+    </main>
+  );
 };
 
 export default MainLayout;
